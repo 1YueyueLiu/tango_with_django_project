26e1919d7251e0dbe6a971856713b318be4d51e5
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-07-28 01:06:13 +0100

    The exercises of chapter3, create a new view method called 'about' . The page of 'about' and 'index' can jump to each other through hyperlinks.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rango import views
+from rango import urls, views
 from django.urls import include
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('rango/',include('rango.urls')),
+    path('rango/', include('rango.urls')),
+    path('about/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
