10a3d66b2cd9018a22a85beb5485cb394e1d1cb0
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-07-29 20:02:01 +0100

    The function of exercise in Chapter5 has been done and fix a bug

--- populate_rango.py
+++ populate_rango.py
@@ -39,13 +39,13 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'],p['views'])
+            add_page(c, p['title'], p['url'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
              print(f'- {c}: {p}')
     
-def add_page(cat, title, url, views):
+def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,12 @@ from typing import ContextManager
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
      #construct a dictionary to pass to the template engine as its context
+     category_list = Category.objects.order_by('-likes')[:5]
      context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
      #return a rendered response to send to the client
      return render(request, 'rango/index.html', context=context_dict)
