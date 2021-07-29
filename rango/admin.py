fc8987a8a92fabac8838748e8f09cabd2a6d7021
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-07-29 18:45:07 +0100

    The exercise of chapter5 has been done

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,11 @@
 from rango.models import Page
 from django.contrib import admin
-from rango.models import Category, Page
-# Register your models here.
+from rango.models import Category
+
+
+class PageAdmin(admin.ModelAdmin):
+      list_disaplay = ('title', 'category', 'url')
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -12,10 +13,10 @@ class Category(models.Model):
         return self.name
         
 class Page(models.Model):
-     category = models.ForeignKey(Category, on_delete=models.CASCADE)
      title = models.CharField(max_length=128)
+     category = models.ForeignKey(Category, on_delete=models.CASCADE)
      url = models.URLField()
      views = models.IntegerField(default=0)
 
      def __str__(self):
-        return self.title
+           return self.title
