240cdeb81e4d9949219bb88d0dd26361b3d1ffa2
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-07-30 16:18:29 +0100

    The final version. After testing from test3 to test10. The function from exercise3 to exercise10 seems to be achieved.

--- rango/admin.py
+++ rango/admin.py
@@ -5,10 +5,10 @@ from rango.models import UserProfile
 
 
 class PageAdmin(admin.ModelAdmin):
-      list_display = ('title', 'category', 'url','views')
+      list_display = ('title', 'category', 'url',)
 class CategoryAdmin(admin.ModelAdmin):
       prepopulated_fields={'slug':('name','views')}
 
-admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(UserProfile)
